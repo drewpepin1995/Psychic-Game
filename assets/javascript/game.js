570aa113f9be1fa739b23dfa16129d9722a80cde
Drew Pepin <drewpepin1995@gmail.com>
2019-07-12 14:13:03 -0400

    Fixing some major JS issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 
-let computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let possibleChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+let computerGuess = '';
 
 let playerWins = 0;
 
@@ -9,22 +11,48 @@ let playerGuesses = 9;
 
 let userGuesses = [];
 
+function resetGame() {
+    playerLosses = 0;
+    playerWins = 0;
+    playerGuesses = 9;
+    userGuesses = [];
+}
+
+function updateComputerGuess () {
+    computerGuess = possibleChoices[Math.floor(Math.random() * possibleChoices.length)]
+};
+
+updateComputerGuess();
+
 
 document.onkeyup = function(event) {
 
+    console.log(computerGuess)
+
     let userGuess = event.key
 
-    let computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    console.log(userGuess)
+
+    if (possibleChoices.indexOf(userGuess) === -1) {
+        alert('Please enter a character [a-z]!');
+        resetGame();
+        updateComputerGuess();
+
+    };
+
 
     if (userGuess === computerGuess) {
         playerGuesses=9;
         playerWins++;
         userGuesses=[];
         alert("Correct! You win this round!")
+        updateComputerGuess();
 
     } else {
         playerGuesses--
         userGuesses.push(userGuess);
+        
+        
         
     };
 
@@ -33,20 +61,17 @@ document.onkeyup = function(event) {
         playerGuesses=playerGuesses+9;
         userGuesses = [];
         alert("Incorrect! You lose this round!")
+        updateComputerGuess();
     };
     if (playerWins === 5) {
         alert("Game Over! You win!")
-        playerLosses = 0;
-        playerWins = 0;
-        userGuesses = [];
-        playerGuesses = 0;
+        resetGame();
+        updateComputerGuess();
     };
      if (playerLosses === 10) {
         alert("Too many losses! BOO! You lose! Game Over!")
-        playerLosses = 0;
-        playerWins = 0;
-        userGuesses = [];
-        playerGuesses = 0;
+        resetGame();
+        updateComputerGuess();
     };
 
 
